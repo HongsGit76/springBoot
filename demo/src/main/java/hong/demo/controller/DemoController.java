fee536a1535370ee5e9ec9c8b40fc603f7bca9b0
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-03-30 14:54:36 +0900

    주석 추가

--- demo/src/main/java/hong/demo/controller/DemoController.java
+++ demo/src/main/java/hong/demo/controller/DemoController.java
@@ -1,12 +1,16 @@
 package hong.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class DemoController {
+    // get /hello 로 들어왔을때
     @GetMapping("hello")
     public String demo(Model model){
         model.addAttribute("data", "hello!!");
+        // return : resources/templates 에서 hello.html을 찾아서 보여줌
         return "hello";
     }
 }
